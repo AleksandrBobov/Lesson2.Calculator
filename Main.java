62cbdfb8c621a04fa76ced8081e2f417776d0468
AleksandrBobov <alexb3939@gmail.com>
2022-05-26 13:57:06 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(4,3));
+        System.out.println(calc.mult(4,6));
+        System.out.println(calc.pow(6,2));
+
+    }
+}
